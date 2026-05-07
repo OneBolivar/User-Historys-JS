2e4fab9458390435c5640238c1b0dd546d8de073
One <jebolivar33@gmail.com>
2026-05-07 14:07:22 -0500

    Hay que terminar el JS

--- Week1/assets/js/sistema_interactivo.js
+++ Week1/assets/js/sistema_interactivo.js
@@ -1,63 +1,33 @@
 //Ejecución de los comandos (generales) al cargar la página
-let nombre =  prompt("Ingrese su nombre:")/*El "prompt" sirve para pedir un dato en js*/
-let Confirmar = confirm("¿Desea continuar?")/*El "confirm" sirve para pedir una confirmacion en js, devuelve un valor booleano (true o false) dependiendo de la respuesta del usuario*/
-if (Confirmar) {
-    alert("Hola" + " " + nombre + " " + "¡Gracias por continuar!")/*El "alert" sirve para mostrar un mensaje en js*/
-}
-else {
-    alert("Hola" + " " + nombre + " " + "¡Gracias por su visita!")/*El "alert" sirve para mostrar un mensaje en js*/
-    window.close()/*El "window.close()" sirve para cerrar la ventana del navegador, pero solo funciona si la ventana fue abierta por un script, no funcionará si el usuario abrió la página directamente*/
-}
-
-//------------------------------------------------------------------------------------------------------------------------------------------
-//Funciones para el menu (general)
-function desplegar() {
-
-    let menu = document.getElementsByClassName("links")//Variable para traer el menu del html(en este caso se llama links)
-    let boton = document.getElementsByClassName("boton-menu")//Variable para traer el boton del html(en este caso se llama boton-menu)
-    //Si el menu esta activo se muestra la X y si no lo esta se muestra el simbolo de las tres rayas
-    if (menu[0].classList.contains("active")) {
-        boton[0].innerText = "≡";
-
-
+let nombre =  prompt("Ingrese su nombre:")/*El let es el */ 
+validadorEdad()
+let validadorGeneral = true
+function validadorEdad(){
+        let edad = prompt("Ingrese su edad: ") /*El "prompt" sirve para pedir un dato en js*/
+        while (validadorGeneral){
+            if (isNaN(edad) || (edad < 18) || (edad < 0)){
+                alert("Error: Por favor, ingresa una edad válida en números.")
+                let edad = prompt("Ingrese su edad: ")
+            }else{
+                validadorGeneral = false
+            }        
+        }
+  
         
-    }
-    else {
-        boton[0].innerText = "X";
-    }
-    //El metodo toggle se encarga de agregar o quitar la clase active dependiendo de si esta o no esta en el elemento
-    menu[0].classList.toggle("active")
-
 }
-//------------------------------------------------------------------------------------------------------------------------------------------
-//Funciones para el boton de "Ver mas" en la seccion de informacion adicional (pagina pets.html)
-function verMas() {
-    let boton = document.getElementsByClassName("boton-ver-mas");
-    let info = document.getElementsByClassName("Info-extra");
-    //Si el boton esta activo se muestra el texto "Ver mas" y si no lo esta se muestra el texto "Ver menos"
-    boton[0].classList.toggle("active")
-    info[0].classList.toggle("active")
-    if (boton[0].classList.contains("active")) {
-        boton[0].innerText = "Ver menos";
 
-    }
-    else {
-        boton[0].innerText = "Ver más";
-    }
-    //El metodo toggle se encarga de agregar o quitar la clase active dependiendo de si esta o no esta en el elemento
 
-}
-//------------------------------------------------------------------------------------------------------------------------------------------
-//Funciones para el boton de "Ver mas" en la seccion de mascotas (pagina pets.html)
-function nombres() {
-    let boton = document.getElementsByClassName("boton-ver-mas");
-    let nombre = document.getElementsByClassName("Nombres");
-    //Si el boton esta activo se muestra el texto "Ver mas" y si no lo esta se muestra el texto "Ver menos"
-    boton[0].classList.toggle("active")
-    for (let index = 0; index < nombre.length; index++) {
-        const element = nombre[index];
-        element.classList.toggle("active")
-    }
 
-}
+
+
+
 //------------------------------------------------------------------------------------------------------------------------------------------
+//(edad != "")?alert("Hola si es una edad"):alert("Ingrese una edad valida") //ESTO ES UN CONDICIONAL TAMBIEN
+// let Confirmar = confirm("¿Desea continuar?")/*El "confirm" sirve para pedir una confirmacion en js, devuelve un valor booleano (true o false) dependiendo de la respuesta del usuario*/
+// if (Confirmar) {
+//     alert("Hola" + " " + nombre + " " + "¡Gracias por continuar!")/*El "alert" sirve para mostrar un mensaje en js*/
+// }
+// else {
+//     alert("Hola" + " " + nombre + " " + "¡Gracias por su visita!")
+//     window.close()/*El "window.close()" sirve para cerrar la ventana del navegador, pero solo funciona si la ventana fue abierta por un script, no funcionará si el usuario abrió la página directamente*/
+// }
