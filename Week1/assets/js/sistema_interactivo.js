9bd80935ef174c9bdb4e5b63ff497b0d642a0d2e
One <jebolivar33@gmail.com>
2026-05-07 15:10:10 -0500

    Semana 1 con alerts lista

--- Week1/assets/js/sistema_interactivo.js
+++ Week1/assets/js/sistema_interactivo.js
@@ -1,21 +1,20 @@
-//Ejecución de los comandos (generales) al cargar la página
-let nombre =  prompt("Ingrese su nombre:")/*El let es el */ 
-validadorNumero()//Esta funncion esta creada para validar la edad
-let validadorGeneral = true //Validador general creado para hacer la funcion de un while true
-function validadorNumero(){
-        let edad = prompt("Ingrese su edad: ") /*El "prompt" sirve para pedir un dato en js*/
-        (edad < 18)?alert("Hola" + nombre +  "eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!"): alert("Hola" + nombre +  "eres mayor de edad. ¡Prepárate para grandes oportunidades en el mundo de la programación!")
-        while (validadorGeneral){//Validador para que se ejecute siempre que se equivoquen
-            if (isNaN(edad)){// Si no es un numero entonces
-                alert("Error: Por favor, ingresa una edad válida en números.")
-                let edad = prompt("Ingrese su edad: ")
-            }else{
-                validadorGeneral = false
-            }        
-        }  
-
+let nombre = prompt("Ingrese su nombre:"); 
+let validadorGeneral = true; //Validador para que funcione como un while true
+function validadorNumero() {//Esta funcion esta creada para validar la edad
+    let edad = prompt("Ingrese su edad: ");
+    while (validadorGeneral) {
+        if (isNaN(edad) || edad === "" || edad === null) { //Si edad no es un numero entonces
+            alert("Error: Por favor, ingresa una edad válida en números."); //Alerta de error
+            edad = prompt("Ingrese su edad: ");//Vuelve a pedir la edad
+        } else {//Sino
+            validadorGeneral = false;//El validador general se vuelve falso y se sale del bucle
+        }
+    }
+    (edad < 18)//Validacion ternaria
+        ? alert("Hola " + nombre + ", eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!")
+        : alert("Hola " + nombre + ", eres mayor de edad. ¡Prepárate para grandes oportunidades en el mundo de la programación!!");
 }
-
+validadorNumero();
 
 
 
