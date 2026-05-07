77307627b961cf4b3fc43606ee16d1fcd3eda1ca
One <jebolivar33@gmail.com>
2026-05-07 14:39:07 -0500

    Hay que arreglar el condicional validador de la edad

--- Week1/assets/js/sistema_interactivo.js
+++ Week1/assets/js/sistema_interactivo.js
@@ -1,19 +1,19 @@
 //Ejecución de los comandos (generales) al cargar la página
 let nombre =  prompt("Ingrese su nombre:")/*El let es el */ 
-validadorEdad()
-let validadorGeneral = true
-function validadorEdad(){
+validadorNumero()//Esta funncion esta creada para validar la edad
+let validadorGeneral = true //Validador general creado para hacer la funcion de un while true
+function validadorNumero(){
         let edad = prompt("Ingrese su edad: ") /*El "prompt" sirve para pedir un dato en js*/
-        while (validadorGeneral){
-            if (isNaN(edad) || (edad < 18) || (edad < 0)){
+        (edad < 18)?alert("Hola" + nombre +  "eres menor de edad. ¡Sigue aprendiendo y disfrutando del código!"): alert("Hola" + nombre +  "eres mayor de edad. ¡Prepárate para grandes oportunidades en el mundo de la programación!")
+        while (validadorGeneral){//Validador para que se ejecute siempre que se equivoquen
+            if (isNaN(edad)){// Si no es un numero entonces
                 alert("Error: Por favor, ingresa una edad válida en números.")
                 let edad = prompt("Ingrese su edad: ")
             }else{
                 validadorGeneral = false
             }        
-        }
-  
-        
+        }  
+
 }
 
 
