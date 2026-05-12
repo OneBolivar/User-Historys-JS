fab1c281a5ddf1107ba3d5408ef79e966f5c2a89
juan <jebolivar33@gmail.com>
2026-05-12 15:03:32 -0500

    Empezando week 2

---
+++ Week2/assets/js/gestion_datos.js
@@ -0,0 +1,5 @@
+let producto = {
+    id : prompt("Ingrese el id del prooducto"),
+    nombre : prompt("Ingrese el nombre del producto"),
+    precio : prompt("Ingrese el precio del producto, caremonda")
+}
