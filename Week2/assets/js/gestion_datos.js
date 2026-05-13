83cd53fa51035cb63ba3433f3cd6b13dd0f7f0cf
juan <jebolivar33@gmail.com>
2026-05-13 14:50:23 -0500

    Punto 1 y 2 terminados

--- Week2/assets/js/gestion_datos.js
+++ Week2/assets/js/gestion_datos.js
@@ -1,5 +1,29 @@
-let producto = {
-    id : prompt("Ingrese el id del prooducto"),
-    nombre : prompt("Ingrese el nombre del producto"),
-    precio : prompt("Ingrese el precio del producto, caremonda")
+//TASK 1 (Completada)
+const listaProductos = [
+  // Objeto creado para almacenar los productos
+  {id: 101,nombre: "Salchicha",precio: 2000},
+  {id: 102,nombre: "Lenteja",precio: 1000}
+];
+
+listaProductos.push({id: 103,nombre: "Telefono",precio: 500000});
+listaProductos.push({id: 104, nombre: "Moto", precio: 7500000})
+console.table(listaProductos);
+
+
+//TASK 2 (Completada)
+let setNumeros = new Set([1, 2, 3, 1, 2, 5]); //Arreglo que no muestra datos repetidos
+setNumeros.add(6);
+let verificacion = setNumeros.has(7);
+console.log("------------------------------")
+console.log("¿EL numero 7 existe en el arreglo?: " + verificacion);
+console.log(setNumeros)
+console.log("------------------------------")
+let borrarNumero = setNumeros.delete(1);
+console.log(" ¿El numero 1 fue borrado correctamente?: " + borrarNumero);
+console.log(setNumeros)
+console.log("------------------------------")
+
+console.log(" Los valores que tiene la lista de numeros son los siguientes: ")
+for (Numero  of setNumeros) {
+  console.log(Numero)
 }
