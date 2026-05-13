b018b3a3ced616d89841f7f4c119b68908d48248
juan <jebolivar33@gmail.com>
2026-05-13 15:47:39 -0500

    Arreglar punto 3

--- Week2/assets/js/gestion_datos.js
+++ Week2/assets/js/gestion_datos.js
@@ -10,20 +10,30 @@ listaProductos.push({id: 104, nombre: "Moto", precio: 7500000})
 console.table(listaProductos);
 
 
-//TASK 2 (Completada)
-let setNumeros = new Set([1, 2, 3, 1, 2, 5]); //Arreglo que no muestra datos repetidos
-setNumeros.add(6);
-let verificacion = setNumeros.has(7);
-console.log("------------------------------")
-console.log("¿EL numero 7 existe en el arreglo?: " + verificacion);
-console.log(setNumeros)
-console.log("------------------------------")
-let borrarNumero = setNumeros.delete(1);
-console.log(" ¿El numero 1 fue borrado correctamente?: " + borrarNumero);
-console.log(setNumeros)
-console.log("------------------------------")
+// //TASK 2 (Completada)
+// let setNumeros = new Set([1, 2, 3, 1, 2, 5]); //Arreglo que no muestra datos repetidos
+// setNumeros.add(6);
+// let verificacion = setNumeros.has(7);
+// console.log("------------------------------")
+// console.log("¿EL numero 7 existe en el arreglo?: " + verificacion);
+// console.log(setNumeros)
+// console.log("------------------------------")
+// let borrarNumero = setNumeros.delete(1);
+// console.log(" ¿El numero 1 fue borrado correctamente?: " + borrarNumero);
+// console.log(setNumeros)
+// console.log("------------------------------")
 
-console.log(" Los valores que tiene la lista de numeros son los siguientes: ")
-for (Numero  of setNumeros) {
-  console.log(Numero)
-}
+// console.log(" Los valores que tiene la lista de numeros son los siguientes: ")
+// for (Numero  of setNumeros) {
+//   console.log(Numero)
+// }
+
+//TASK 3 (Arreglar problema de clave repetida, ya sabemos que es por el set)
+const mapCategorias = new Map();
+mapCategorias.set("Comida", listaProductos[0].nombre)
+mapCategorias.set("Comida", listaProductos[1].nombre)
+mapCategorias.set("Dispositivos", listaProductos[2].nombre)
+mapCategorias.set("Vehiculo", listaProductos[3].nombre)
+
+console.table(mapCategorias);
+console.error("La tabla tiene un error, debe mostrar los datos de comida sin que se eliminen")
